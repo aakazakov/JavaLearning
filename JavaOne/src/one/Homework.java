c32f8b901ffb1a85c565efdf543e2b764601e886
andrey_kazakov <aakazakovv@yandex.ru>
2020-02-07 16:48:58 +0300

    ex. 1,2 done

--- JavaOne/src/one/Homework.java
+++ JavaOne/src/one/Homework.java
@@ -2,9 +2,34 @@ package one;
 
 public class Homework {
 
+  /**
+   * Run exercises.
+   * 
+   * @param args arguments
+   */
   public static void main(String[] args) {
-    System.out.println("hello world");
-
+    Homework.printVaraibles();
   }
-
+  
+  /** #1. */
+  public static void printVaraibles() {
+    byte myByte = 2;
+    short myshort = 15;
+    int myInt = 100;
+    long myLong = 101010L;
+    float myFloat = 12.6f;
+    double myDouble = 12.6547898;
+    char myChar = 'c';
+    String myString = "hi";   
+    System.out.println(
+        "myByte = " + myByte
+        + "\nmyshort = " + myshort
+        + "\nmyInt = " + myInt
+        + "\nmyLong = " + myLong
+        + "\nmyFloat = " + myFloat
+        + "\nmyDouble = " + myDouble
+        + "\nmyChar = " + myChar
+        + "\nmyString = " + myString
+    );
+  }
 }
