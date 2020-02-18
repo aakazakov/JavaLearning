7504531e042696073aa1ee7b950b0f2c2a3856a3
andrey_kazakov <aakazakovv@yandex.ru>
2020-02-18 16:00:14 +0300

    add fields

--- JavaOne/src/four/Homework.java
+++ JavaOne/src/four/Homework.java
@@ -1,11 +1,24 @@
 package four;
 
 public class Homework {
+  private static final int SIZE = 3;
+  private static final char[][] MAP = new char[SIZE][SIZE];
+  private static final char CHAR_DOT = 176;
+  private static final char CHAR_X = 'X';
+  private static final char CHAR_Y = 'Y';
 
+  /**
+   * The main method.)
+   * 
+   * @param args arguments
+   */
   public static void main(String[] args) {
     ticTacToe();
   }
   
+  /**
+   * TicTacToe game.
+   */
   public static void ticTacToe() {
     System.out.println("Game started!");
   }
