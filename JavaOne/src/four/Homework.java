dc91267f601da384261e8cada85cb4c7877ac18c
andrey_kazakov <aakazakovv@yandex.ru>
2020-02-18 15:31:03 +0300

    ex 1 start

---
+++ JavaOne/src/four/Homework.java
@@ -0,0 +1,12 @@
+package four;
+
+public class Homework {
+
+  public static void main(String[] args) {
+    ticTacToe();
+  }
+  
+  public static void ticTacToe() {
+    System.out.println("Game started!");
+  }
+}
