e8fabb788c7784d2b672774f832e3df89e88c4c6
andrey_kazakov <aakazakovv@yandex.ru>
2020-02-18 16:46:00 +0300

    add map visualization

--- JavaOne/src/four/Homework.java
+++ JavaOne/src/four/Homework.java
@@ -1,11 +1,13 @@
 package four;
 
+import java.util.Arrays;
+
 public class Homework {
   private static final int SIZE = 3;
   private static final char[][] MAP = new char[SIZE][SIZE];
-  private static final char CHAR_DOT = 176;
+  private static final char CHAR_SHARP = 35;
   private static final char CHAR_X = 'X';
-  private static final char CHAR_Y = 'Y';
+  private static final char CHAR_O = 'O';
 
   /**
    * The main method.)
@@ -21,5 +23,27 @@ public class Homework {
    */
   public static void ticTacToe() {
     System.out.println("Game started!");
+    fillTheMap();
+    printTheMap();
   }
+  
+  private static void fillTheMap() {
+    for (int i = 0; i < SIZE; i++) {
+      Arrays.fill(MAP[i], CHAR_SHARP);
+    }
+  }
+  
+  private static void printTheMap() {
+    for (int i = 0; i <= SIZE; i++) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+    for (int i = 0; i < SIZE; i++) {
+      System.out.print(i + 1 + " ");
+      for (int j = 0; j < SIZE; j++) {
+        System.out.print(MAP[i][j] + " ");
+      }
+      System.out.println();
+    }
+  } 
 }
