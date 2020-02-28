e482bf48cb3402318cef00795d43816ba19e19ae
andrey_kazakov <aakazakovv@yandex.ru>
2020-02-28 21:40:48 +0300

    fill the class

--- JavaOne/src/six/Animal.java
+++ JavaOne/src/six/Animal.java
@@ -1,5 +1,33 @@
 package six;
 
 public class Animal {
-
+  String name;
+  int yearOfBirth;
+  int maxRunDistance = 0;
+  int maxSwimDistance = 0;
+  int maxJumpHeight = 0;
+  
+  public Animal(String name, int yearOfBirth) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+  }
+   
+  public boolean run(int distance) {
+    return distance <= maxRunDistance;
+  }
+  
+  public boolean swim(int distance) {
+    return distance <= maxSwimDistance;
+  }
+  
+  public boolean jumpOverAnObstacle(int height) {
+    return height <= maxJumpHeight;
+  }
+  
+  public void setPhysicalPowerParameters(int maxRunDistance,
+      int maxSwimDistance, int maxJumpHeight) {
+    this.maxRunDistance = maxRunDistance;
+    this.maxSwimDistance = maxSwimDistance;
+    this.maxJumpHeight = maxJumpHeight;
+  }
 }
