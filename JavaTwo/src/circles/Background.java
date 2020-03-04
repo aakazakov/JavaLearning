67232bc056491e457e6c38aaea749749b2468f41
andrey_kazakov <aakazakovv@yandex.ru>
2020-03-04 22:19:16 +0300

    override methods

--- JavaTwo/src/circles/Background.java
+++ JavaTwo/src/circles/Background.java
@@ -1,19 +1,26 @@
 package circles;
 
-import java.awt.Graphics;
+import java.awt.*;
 
 public class Background extends Sprite {
+  private Color color;
+  private float deltaTime;
+  
+  public Background(float deltaTime) {
+    color = getRandomColor();
+    this.deltaTime = deltaTime;
+  }
 
   @Override
   public void update(MainCanvas canvas, float deltaTime) {
-    // TODO Auto-generated method stub
-    
+    x = halfWidth;
+    y = halfHeight;
   }
 
   @Override
   public void render(MainCanvas canvas, Graphics g) {
-    // TODO Auto-generated method stub
-    
+    g.setColor(color);
+    g.fillRect((int) x, (int) y, (int) getWidth(), (int) getHeight());
   }
 
 }
