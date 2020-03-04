6eb5c39e7070611170b062939c92c8bef1ff1ebd
andrey_kazakov <aakazakovv@yandex.ru>
2020-03-04 22:00:45 +0300

    add an empty background class

---
+++ JavaTwo/src/circles/Background.java
@@ -0,0 +1,19 @@
+package circles;
+
+import java.awt.Graphics;
+
+public class Background extends Sprite {
+
+  @Override
+  public void update(MainCanvas canvas, float deltaTime) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void render(MainCanvas canvas, Graphics g) {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
