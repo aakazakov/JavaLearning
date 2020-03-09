4e60c4a0c0a8c8d8c9935faefea982cb0d513490
andrey_kazakov <aakazakovv@yandex.ru>
2020-03-09 23:32:58 +0300

    add default constructor

--- JavaTwo/src/practice/NotAnIntegerException.java
+++ JavaTwo/src/practice/NotAnIntegerException.java
@@ -1,7 +1,11 @@
 package practice;
 
-public class NotAnIntegerException extends ArithmeticException {
+public class NotAnIntegerException extends NumberFormatException {
   public NotAnIntegerException(String errorMessage) {
     super(errorMessage + " can't convert to integer");
+  }
+  
+  public NotAnIntegerException() {
+    
   }
 }
