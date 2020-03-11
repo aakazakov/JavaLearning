f6310d6ea0f4b5769d8ed522f39e20f933ee6fa0
andrey_kazakov <aakazakovv@yandex.ru>
2020-03-11 18:57:40 +0300

    ex 1.2 done

--- JavaTwo/src/practice1/Homework.java
+++ JavaTwo/src/practice1/Homework.java
@@ -1,23 +1,46 @@
 package practice1;
 
-import java.util.Set;
+import java.util.*;
 
 public class Homework {
-  static final String[] words = {
+  static final String[] someWords = {
       "apple", "orange", "banana", "pineapple", "grape", "watermelon",
       "coconut", "juice", "mandarin", "berry", "tea", "coffe", "grape",
       "mango", "pineapple", "coconut", "melon", "melon", "tea", "coffe",
       "apple", "orange", "banana", "pineapple", "grape", "watermelon",
       "coconut", "juice", "mandarin", "berry", "tea", "coffe", "grape",
-      "mango", "pineapple", "coconut", "melon", "melon", "tea", "coffe"
+      "mango", "pineapple", "coconut","tea", "coffe", "strawberry"
   };
-
+  
   public static void main(String[] args) {
     
+    // ex. 1.
+    
     Words w = new Words();
-    Set<String> unique = w.getUniqueWords(words);
-    System.out.println(unique);
+    
+    Set<String> uniqueWords = w.getUniqueWords(someWords);
+    Map<String, Integer> wordsWithScore = w.getRepetitionOfWords(someWords);
+    
+    System.out.println(uniqueWords);
+    printRepetitionOfWords(wordsWithScore);
+    
+    // ex. 2.
+    
+    
     
   }
-
+  
+  public static void printRepetitionOfWords(Map<String, Integer> m) {
+    Set<Map.Entry<String, Integer>> entries = m.entrySet();
+    Iterator<Map.Entry<String, Integer>> i = entries.iterator();
+    while (i.hasNext()) {
+      Map.Entry<String, Integer> entry = i.next();
+      if (entry.getValue() > 1) {
+        System.out.printf("'%s' is repeated %d times.%n", entry.getKey(), entry.getValue());
+      } else {
+        System.out.printf("'%s' has no repetition.%n", entry.getKey());
+      }
+    }
+  }
+  
 }
--- JavaTwo/src/practice1/Words.java
+++ JavaTwo/src/practice1/Words.java
@@ -4,14 +4,38 @@ import java.util.*;
 
 public class Words {
   private Set<String> uniqueWords = new HashSet<>();
+  private Map<String, Integer> wordsMap = new HashMap<>();
   
   public Set<String> getUniqueWords(String[] arr) {
-    uniqueWords(arr);
+    fillUniqueWords(arr);
     return uniqueWords;
   }
   
-  private void uniqueWords(String[] arr) {
-    Collections.addAll(uniqueWords, arr);
+  private void fillUniqueWords(String[] arr) {
+    try {
+      Collections.addAll(uniqueWords, arr);
+    } catch (RuntimeException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  public Map<String, Integer> getRepetitionOfWords(String[] arr) {
+    fillWordsMap(arr);
+    return wordsMap;
+  }
+  
+  private void fillWordsMap(String[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      try {
+        if (wordsMap.containsKey(arr[i])) {
+          wordsMap.put(arr[i], wordsMap.get(arr[i]) + 1);
+        } else {
+          wordsMap.put(arr[i], 1);
+        }
+      } catch (RuntimeException e) {
+          e.printStackTrace();
+      }
+    }
   }
 
 }
