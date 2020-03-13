cf4f0809deafcdb859463fb07ca9d66f695850ea
andrey_kazakov <aakazakovv@yandex.ru>
2020-03-13 12:03:46 +0300

    add phonebook class

---
+++ JavaTwo/src/practice1/PhoneBook.java
@@ -0,0 +1,11 @@
+package practice1;
+
+import java.util.*;
+
+public class PhoneBook {
+  public static final String EMAIL = "email";
+  public static final String PHONE = "phone";
+  
+  private Map<String, Map<String, List<String>>> phoneBook;
+
+}
