56559f790ee9b1df28d179725513bd6559381835
andrey_kazakov <aakazakovv@yandex.ru>
2020-03-16 09:37:09 +0300

    start global code rewriting ))

--- JavaTwo/src/practice1/Person.java
+++ JavaTwo/src/practice1/Person.java
@@ -1,51 +1,13 @@
 package practice1;
 
-import java.util.*;
-
 public class Person {
-  private String surname;
-  private Map<String, List<String>> contacts = new HashMap<>();
-  private List<String> emails = new ArrayList<>();
-  private List<String> phones = new ArrayList<>();
+
+  private String email;
+  private String phone;
   
-  public Person(String surname, String[] emails, String[] numbers) {
-    this.surname = surname.toLowerCase();
-    addToEmail(emails);
-    addToPhone(numbers);
-  }
-  
-  private void addToEmail(String[] addresses) {
-    try {
-      emails.addAll(Arrays.asList(addresses));
-    } catch (RuntimeException e) {
-      e.printStackTrace();
-    }
-  }
-  
-  private void addToPhone(String[] numbers) {
-    try {
-      phones.addAll(Arrays.asList(numbers));
-    } catch (RuntimeException e) {
-      e.printStackTrace();
-    }
-  }
-  
-  public Map<String, List<String>> getContacts() {
-    fillContacts();
-    return contacts;
-  }
-  
-  private void fillContacts() {
-    try {
-      contacts.put(PhoneBook.EMAIL, emails);
-      contacts.put(PhoneBook.PHONE, phones);
-    } catch (RuntimeException e) {
-      e.printStackTrace();
-    }
-  }
-  
-  public String getSurname() {
-    return surname;
+  public Person(String email, String phone) {
+    this.email = email;
+    this.phone = phone;
   }
   
 }
--- JavaTwo/src/practice1/PhoneBook.java
+++ JavaTwo/src/practice1/PhoneBook.java
@@ -3,36 +3,16 @@ package practice1;
 import java.util.*;
 
 public class PhoneBook {
-  public static final String EMAIL = "email";
-  public static final String PHONE = "phone";
-  
-  private Map<String, Map<String, List<String>>> phoneBookEntries = new HashMap<>();
-  
-  public void addToPhoneBook(Person p) {
-    try {
-      phoneBookEntries.put(p.getSurname(), p.getContacts());
-    } catch (RuntimeException e) {
-      e.printStackTrace();
+  Map<String, List<Person>> entries;
+
+  public void addToPhoneBook(String surname, String phone, String email) {
+    if (entries.containsKey(surname)) {
+      List<Person> person = entries.get(surname);
+      person.add(new Person(phone, email));
+    } else {
+      List<Person> person = new ArrayList<>();
+      person.add(new Person(phone, email));
+      entries.put(surname, person);
     }
-  }
-  
-  public void printInfo(String surname, String param) {
-    surname = surname.toLowerCase();
-    List<String> result = null;
-    if (param.equals(EMAIL)) {
-      result = getEmail(surname);
-    }
-    if (param.equals(PHONE)) {
-      result = getPhone(surname);
-    }
-    System.out.println(result);
-  }
-  
-  private List<String> getEmail(String surname) {
-    return phoneBookEntries.get(surname).get(EMAIL);
-  }
-  
-  private List<String> getPhone(String surname) {
-    return phoneBookEntries.get(surname).get(PHONE);
   }
 }
