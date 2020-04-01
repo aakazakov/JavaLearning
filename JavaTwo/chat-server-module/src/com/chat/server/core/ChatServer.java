0eae7b0399952070bc57d4753cbbbeed2f31bd4d
andrey_kazakov <aakazakovv@yandex.ru>
2020-04-01 14:24:52 +0300

    add set of clients

--- JavaTwo/chat-server-module/src/com/chat/server/core/ChatServer.java
+++ JavaTwo/chat-server-module/src/com/chat/server/core/ChatServer.java
@@ -2,13 +2,15 @@ package com.chat.server.core;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Vector;
 
+import com.chat.library.Library;
 import com.chat.network.*;
 
 public class ChatServer implements ServerSocketThreadListener, SocketThreadListener {
   private ServerSocketThread server;
   private ChatServerListener listener;
-  
+  private Vector<SocketThread> clients = new Vector<>();
   public ChatServer(ChatServerListener listener) {
     this.listener = listener;
   }
@@ -71,22 +73,52 @@ public class ChatServer implements ServerSocketThreadListener, SocketThreadListe
   @Override
   public void onSocketReady(SocketThread thread, Socket socket) {
     putLog("Client is ready to chat");
+    clients.add(thread);
   }
-
-  @Override
-  public void onReceivedString(SocketThread thread, Socket socket, String msg) {
-    ClientThread client = (ClientThread) thread;
-    client.sendMessage(msg);
-  }
-
+  
   @Override
   public void onSocketStopped(SocketThread thread) {
     putLog("Client disconnected");
+    clients.remove(thread);
   }
 
   @Override
   public void onSocketException(SocketThread thread, Throwable e) {
     putLog("Oops! Exception happend");
     e.printStackTrace();
+  }
+
+  @Override
+  public void onReceivedString(SocketThread thread, Socket socket, String msg) {
+    ClientThread client = (ClientThread) thread;
+    if (client.isAuthorized()) {
+      handleAuthorizedMessage(client, msg);
+    } else {
+      handleNonAuthorizedMassage(client, msg);
+    }
+    client.sendMessage(msg);
+  }
+  
+  private void handleNonAuthorizedMassage(ClientThread client, String msg) {
+    String[] arr = msg.split(Library.DELIMITER);
+    if (arr.length != 3 || !arr[0].equals(Library.AUTH_REQUEST)) {
+      client.msgFormatError(msg);
+      return;
+    }
+    String login = arr[1];
+    String password = arr[2];
+    // SQL HERE
+  }
+  
+  private void handleAuthorizedMessage(ClientThread client, String msg) {
+    sendToAllAuthorizedClients(msg);
+  }
+  
+  private void sendToAllAuthorizedClients(String msg) {
+    for (int i = 0; i < clients.size(); i++) {
+      ClientThread client = (ClientThread) clients.get(i);
+      if (!client.isAuthorized()) continue;
+      client.sendMessage(msg);
+    }
   }
 }
