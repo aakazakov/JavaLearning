784afc4b3c89dafd3ddb38f2bdfd3866d6fabc11
andrey_kazakov <aakazakovv@yandex.ru>
2020-04-03 15:20:16 +0300

    #2 done

--- JavaThree/src/homework/hw1/Homework.java
+++ JavaThree/src/homework/hw1/Homework.java
@@ -11,16 +11,21 @@ public class Homework {
     Methods method = new Methods();
     
     // #1.
-    String[] arr = new String[] {"a", "b" , "c" , "d", "e"};  
-    System.out.println("Before: " + Arrays.toString(arr));
+    String[] arrStr = new String[] {"a", "b" , "c" , "d", "e"};  
+    System.out.println("Before: " + Arrays.toString(arrStr));
     
-    method.swap(arr, 0, 4);
-    System.out.println("After: " + Arrays.toString(arr));
+    method.swap(arrStr, 0, 4);
+    System.out.println("After: " + Arrays.toString(arrStr));
     
-    List<String> list = Arrays.asList(arr);
+    List<String> list = Arrays.asList(arrStr);
     method.swap(list, 1, 3);
     System.out.println("And after another swap: " + list);
     
+    // #2.
+    Integer[] arrInt = new Integer[] {1, 2, 3, 4, 5};
+    ArrayList<Integer> listInt = method.makeArrayList(arrInt);
+    System.out.println("Form Array to ArrayList: " + listInt);
+     
   }
 
 }
--- JavaThree/src/homework/hw1/methods/Methods.java
+++ JavaThree/src/homework/hw1/methods/Methods.java
@@ -17,4 +17,17 @@ public class Methods {
     arr.set(index2, temp);
   }
   
+  // ћожно было указать в качестве параметра массив примитивов,
+  // но тогда пришлось бы €вным образом указывать значение “.
+  // я посмотрел реализацию метода Arrays.asList, там переданный массив
+  // преобразуетс€ в последовательность аргументов (T ...arr). Ќе пойму, почему 
+  // в случае с (T ...arr) можно передать массив примитивов, а в моЄм случае нет).
+  public <T> ArrayList<T> makeArrayList(T[] arr) {
+    ArrayList<T> arrList = new ArrayList<>(arr.length);
+    for (int i = 0; i < arr.length; i++) {
+      arrList.add(arr[i]);
+    }
+    return arrList;
+  }
+  
 }
