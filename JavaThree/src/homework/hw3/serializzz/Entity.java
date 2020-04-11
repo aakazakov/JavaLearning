ba7dc4eb6b9971bd199167e6b855c182ba1c33b8
andrey_kazakov <aakazakovv@yandex.ru>
2020-04-11 15:25:53 +0300

    entity ready

--- JavaThree/src/homework/hw3/serializzz/Entity.java
+++ JavaThree/src/homework/hw3/serializzz/Entity.java
@@ -3,5 +3,23 @@ package homework.hw3.serializzz;
 import java.io.Serializable;
 
 public class Entity implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private String name;
+  private String shape;
+  private String color;
+  private int weight;
+  
+  public Entity(String name, String shape, String color, int weight) {
+    this.name = name;
+    this.shape = shape;
+    this.color = color;
+    this.weight = weight;
+  }
+  
+  @Override
+  public String toString() {
+    return String.format("This is a certain entity called %s, %s shape, %s color, weighs %d",
+        name, shape, color, weight);
+  }
   
 }
