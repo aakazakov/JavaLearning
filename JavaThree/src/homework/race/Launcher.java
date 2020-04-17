5a1f87f64d3c2b8ab0cbc0f4c131bcfb61742e3c
andrey_kazakov <aakazakovv@yandex.ru>
2020-04-17 15:46:06 +0300

    tunnel stage ready

--- JavaThree/src/homework/race/Launcher.java
+++ JavaThree/src/homework/race/Launcher.java
@@ -1,18 +1,28 @@
 package homework.race;
 
+import java.util.concurrent.*;
+
 public class Launcher {
   static final int NUMBER_OF_CARS = 4;
 
   public static void main(String[] args) {
     System.out.println("== Preparation ==");
+    
     Race race = new Race(new Road(60), new Tunnel(), new Road(40));
-    Car[] cars = new Car[NUMBER_OF_CARS];
+    Car[] cars = new Car[NUMBER_OF_CARS];   
+    ExecutorService pool = Executors.newFixedThreadPool(NUMBER_OF_CARS, new ThreadFactory() {
+      @Override
+      public Thread newThread(Runnable r) {
+        return new Thread(r);
+      }
+    });
     for (int i = 0; i < cars.length; i++) {
       cars[i] = new Car(race, 20 + (int) (Math.random() * 10), "Car #" + (i + 1));
     }
     for (int i = 0; i < cars.length; i++) {
-      new Thread(cars[i]).start();
+      pool.execute(cars[i]);
     }
+    pool.shutdown();
     System.out.println("== Race started ==");
     System.out.println("== Race ended ==");
   }
--- JavaThree/src/homework/race/Tunnel.java
+++ JavaThree/src/homework/race/Tunnel.java
@@ -1,6 +1,9 @@
 package homework.race;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
+  private Semaphore semaphore = new Semaphore((int)Launcher.NUMBER_OF_CARS / 2);
   
   public Tunnel() {
     this.length = 80;
@@ -12,12 +15,14 @@ public class Tunnel extends Stage {
     try {
       try {
         System.out.println(car.getName() + " is getting started stage (waiting...): " + description);
+        semaphore.acquire();
         System.out.println(car.getName() + " started stage: " + description);
         Thread.sleep(length / car.getSpeed() * 1000);
       } catch (InterruptedException e) {
         e.printStackTrace();
       } finally {
         System.out.println(car.getName() + " ended stage: " + description);
+        semaphore.release();
       }
     } catch (Exception e) {
       e.printStackTrace();
