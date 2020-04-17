e1a7f60245f128f419e8439d82fd00df4167ee33
andrey_kazakov <aakazakovv@yandex.ru>
2020-04-17 18:23:15 +0300

    done

--- JavaThree/src/homework/race/Launcher.java
+++ JavaThree/src/homework/race/Launcher.java
@@ -6,11 +6,6 @@ public class Launcher {
   static final int NUMBER_OF_CARS = 4;
 
   public static void main(String[] args) {
-    System.out.println("== Preparation ==");
-    
-    Race race = new Race(new Road(60), new Tunnel(), new Road(40), new Finish());
-    Car[] cars = new Car[NUMBER_OF_CARS]; 
-    
     ExecutorService pool = Executors.newFixedThreadPool(NUMBER_OF_CARS, new ThreadFactory() {
       
       @Override
@@ -18,10 +13,14 @@ public class Launcher {
         return new Thread(r);
       }
     });
+    Race race = new Race(new Road(60), new Tunnel(), new Road(40), new Finish());
+    Car[] cars = new Car[NUMBER_OF_CARS]; 
     
     for (int i = 0; i < cars.length; i++) {
       cars[i] = new Car(race, 20 + (int) (Math.random() * 10), "Car #" + (i + 1));
     }
+    
+    System.out.println("== Preparation ==");
     
     for (int i = 0; i < cars.length; i++) {
       pool.execute(cars[i]);
