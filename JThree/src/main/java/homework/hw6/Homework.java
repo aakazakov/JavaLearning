11d3ab5156166272df015a95c938f8ab3a27977b
andrey_kazakov <aakazakovv@yandex.ru>
2020-04-24 15:50:23 +0300

    start write tester

--- JThree/src/main/java/homework/hw6/Homework.java
+++ JThree/src/main/java/homework/hw6/Homework.java
@@ -1,9 +1,17 @@
 package homework.hw6;
 
+import homework.hw6.testing.Tester;
+
 public class Homework {
 
   public static void main(String[] args) {
     
+    try {
+      Tester.start("homework.hw6.TestableClass");
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+      
   }
 
 }
--- JThree/src/main/java/homework/hw6/testing/Tester.java
+++ JThree/src/main/java/homework/hw6/testing/Tester.java
@@ -1,5 +1,16 @@
 package homework.hw6.testing;
 
-public class Tester {
+import java.lang.reflect.*;
+import java.util.Arrays;
 
+public class Tester {
+  
+  public static void start(String className) throws ClassNotFoundException {
+    Class<?> testClass = Class.forName(className);
+    
+    Method[] methods = testClass.getDeclaredMethods();
+    for (Method m : methods) {
+      System.out.println(m.getName() + " " + Arrays.toString(m.getAnnotations()));
+    }
+  }
 }
