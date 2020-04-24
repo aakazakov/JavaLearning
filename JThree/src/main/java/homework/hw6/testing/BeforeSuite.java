652f6e8a2add28534372f3afdaf1bc9151c3d071
andrey_kazakov <aakazakovv@yandex.ru>
2020-04-24 15:49:27 +0300

    change target

--- JThree/src/main/java/homework/hw6/testing/BeforeSuite.java
+++ JThree/src/main/java/homework/hw6/testing/BeforeSuite.java
@@ -1,11 +1,11 @@
 package homework.hw6.testing;
 
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Retention(RUNTIME)
-@Target(CONSTRUCTOR)
+@Target(METHOD)
 public @interface BeforeSuite { }
