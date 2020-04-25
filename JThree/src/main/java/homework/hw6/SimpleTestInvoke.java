38b66f5e518703a39305b7c60c07f50074fb311e
andrey_kazakov <aakazakovv@yandex.ru>
2020-04-25 23:39:10 +0300

    start to start )

--- JThree/src/main/java/homework/hw6/SimpleTestInvoke.java
+++ JThree/src/main/java/homework/hw6/SimpleTestInvoke.java
@@ -1,16 +1,42 @@
 package homework.hw6;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 
 public class SimpleTestInvoke {
   
-  public static void start(String className) throws ClassNotFoundException {
+  public static void start(String className)
+      throws ClassNotFoundException, IllegalAccessException, InvocationTargetException {
     Class<?> testClass = Class.forName(className);
-    
     Method[] methods = testClass.getDeclaredMethods();
+    Robot robot = new Robot();
+    
+    startBeforeSuite(methods, robot);
+    
     for (Method m : methods) {
-      System.out.println(m.getName() + " " + Arrays.toString(m.getAnnotations()));
+      if (m.isAnnotationPresent(Test.class)) {
+        m.invoke(robot);
+      }
+    }
+    
+    for (Method m : methods) {
+      if (m.isAnnotationPresent(AfterSuite.class)) {
+        m.invoke(robot);
+      }
+    }
+    
+  }
+  
+  private static void startBeforeSuite(Method[] methods, Object obj)
+      throws IllegalAccessException, InvocationTargetException {
+    boolean wasInvoked = false;
+    for (Method m : methods) {
+      if (m.isAnnotationPresent(BeforeSuite.class)) {
+        if (wasInvoked) { throw new RuntimeException("BeforeSuite occurs more than 1 time"); }
+        m.setAccessible(true);
+        m.invoke(obj);
+        wasInvoked = true;
+      }
     }
   }
   
