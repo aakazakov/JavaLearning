4976accf3f906b870bfec598ae05d3610dcad32f
andrey_kazakov <aakazakovv@yandex.ru>
2020-04-26 09:02:24 +0300

    universalized alittle

--- JThree/src/main/java/homework/hw6/SimpleTestInvoke.java
+++ JThree/src/main/java/homework/hw6/SimpleTestInvoke.java
@@ -1,40 +1,44 @@
 package homework.hw6;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.*;
 
 public class SimpleTestInvoke {
+  private static Class<?> testClass;
+  private static Method[] methods;
+  private static Object object;
   
   public static void start(String className)
-      throws ClassNotFoundException, IllegalAccessException, InvocationTargetException {
-    Class<?> testClass = Class.forName(className);
-    Method[] methods = testClass.getDeclaredMethods();
-    Robot robot = new Robot();
+      throws ClassNotFoundException,
+      IllegalAccessException,
+      InvocationTargetException,
+      NoSuchMethodException,
+      InstantiationException {
+    testClass = Class.forName(className);
+    Constructor<?> constructor = testClass.getConstructor();
+    object = constructor.newInstance();
+    methods = testClass.getDeclaredMethods();
     
-    startBeforeSuite(methods, robot);
+    launchOnce(BeforeSuite.class);
     
     for (Method m : methods) {
       if (m.isAnnotationPresent(Test.class)) {
-        m.invoke(robot);
+        m.invoke(object);
       }
     }
     
-    for (Method m : methods) {
-      if (m.isAnnotationPresent(AfterSuite.class)) {
-        m.invoke(robot);
-      }
-    }
+    launchOnce(AfterSuite.class);
     
   }
   
-  private static void startBeforeSuite(Method[] methods, Object obj)
+  private static void launchOnce(Class<? extends Annotation> annotationClass)
       throws IllegalAccessException, InvocationTargetException {
     boolean wasInvoked = false;
     for (Method m : methods) {
-      if (m.isAnnotationPresent(BeforeSuite.class)) {
-        if (wasInvoked) { throw new RuntimeException("BeforeSuite occurs more than 1 time"); }
+      if (m.isAnnotationPresent(annotationClass)) {
+        if (wasInvoked) { throw new RuntimeException("Annotation occurs more than 1 time"); }
         m.setAccessible(true);
-        m.invoke(obj);
+        m.invoke(object);
         wasInvoked = true;
       }
     }
