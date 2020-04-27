a3e44ec0732989b9572618a8f056a6989c8a13ad
andrey_kazakov <aakazakovv@yandex.ru>
2020-04-27 20:53:10 +0300

    half-life

--- JThree/src/main/java/homework/spiral/Homework.java
+++ JThree/src/main/java/homework/spiral/Homework.java
@@ -6,149 +6,66 @@ public class Homework {
   
   public static class Spiral {
     
-    int[][] getSpiralOfNumbers(int size) {
-      int[][] matrix = new int[size][size];
+    int[][] getSpiralOfNumbers(int[][] matrix, int counter, int x, int y, int delta) {
       
-      int counter = 1;
+      System.out.println(x + " " + y + " " + counter + " " + delta);
       
-      for (int[] row : matrix) {
-        Arrays.fill(row, 0);
+      if ( x > 0 
+          && y > 0 
+          && x < matrix.length 
+          && y < matrix.length 
+          && matrix[y][x - 1] != 0 
+          && matrix[y][x + 1] != 0 
+          && matrix[y - 1][x] != 0 
+          && matrix[y + 1][x] != 0) {
+        matrix[y][x] = counter;
+        return matrix;        
       }
       
-      int x = 0;
-      int y = 0;
-      
       // left -> right
-      while (true) {
-        
-        if (matrix[y][x] != 0) break;
+      while (x < matrix[y].length - delta) {
         
         matrix[y][x] = counter;
         
         x++;
         counter++;
         
-        if (x == matrix[y].length - 1) break;
       }
            
       // up -> down
-      while (true) {
-        
-        if (matrix[y][x] != 0) break;
+      while (y < matrix.length - delta) {
         
         matrix[y][x] = counter;
         
         y++;
         counter++;
         
-        if (y == matrix.length - 1) break;
-        
       }
       
       // right -> left
-      while (true) {
-        
-        if (matrix[y][x] != 0) break;
+      while (x > delta - 1) {
         
         matrix[y][x] = counter;
         
         x--;
         counter++;
         
-        if (x == 0) break;
-        
       }
       
       // down -> up
-      while (true) {
-        
-        if (matrix[y][x] != 0) break;
-        
+      while (y > delta - 1) {
+                
         matrix[y][x] = counter;
         
         y--;
         counter++;
         
-        if (y == 0) break;
-        
       }
       
-      x++; y++;
+      ++x; ++y; ++delta;
       
+      return getSpiralOfNumbers(matrix, counter, x, y, delta);
       
-      // left -> right
-      while (true) {
-        
-        if (matrix[y][x] != 0) break;
-        
-        matrix[y][x] = counter;
-        
-        x++;
-        counter++;
-        
-        if (x == matrix[y].length - 2) break;
-        
-      }
-           
-      // up -> down
-      while (true) {
-        
-        if (matrix[y][x] != 0) break;
-        
-        matrix[y][x] = counter;
-        
-        y++;
-        counter++;
-        
-        if (y == matrix[y].length - 2) break;
-        
-      }
-      
-      // right -> left
-      while (true) {
-        
-        if (matrix[y][x] != 0) break;
-        
-        matrix[y][x] = counter;
-        
-        x--;
-        counter++;
-        
-        if (x == 1) break;
-        
-      }
-      
-      // down -> up
-      while (true) {
-        
-        if (matrix[y][x] != 0) break;
-        
-        matrix[y][x] = counter;
-        
-        y--;
-        counter++;
-        
-        if (y == 1) break;
-        
-      }
-      
-      x++; y++;
-      
-      // left -> right
-      while (true) {
-        
-        if (matrix[y][x] != 0) break;
-        
-        matrix[y][x] = counter;
-        
-        x++;
-        counter++;
-        
-        if (x == matrix[y].length - 3) break;
-        
-      }
-      
-      return matrix;
     }
     
   }
@@ -156,11 +73,20 @@ public class Homework {
   public static void main(String[] args) {
 
     Spiral spiral = new Spiral();
-    int[][] matrix = spiral.getSpiralOfNumbers(5);
     
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[i].length; j++) {
-        System.out.print(matrix[i][j] + " ");
+    int size = 7;
+    
+    int[][] square = new int[size][size];
+    
+    for (int[] row : square) {
+      Arrays.fill(row, 0);
+    }
+    
+    spiral.getSpiralOfNumbers(square, 1, 0, 0, 1);
+    
+    for (int i = 0; i < square.length; i++) {
+      for (int j = 0; j < square[i].length; j++) {
+        System.out.printf("%2d ", square[i][j]);
       }
       System.out.println();
     }
